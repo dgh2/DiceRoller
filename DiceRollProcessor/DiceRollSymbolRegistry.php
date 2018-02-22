89f16c3c5e23b27d3ecc14a0011c1bed5742cc5e
GHolt <2525915+dgh2@users.noreply.github.com>
2018-02-21 22:16:45 -0600

    Add top, bottom, sum, and stats functions. Refactor quotes

--- DiceRollProcessor/DiceRollSymbolRegistry.php
+++ DiceRollProcessor/DiceRollSymbolRegistry.php
@@ -54,14 +54,14 @@ class DiceRollSymbolRegistry extends SymbolRegistry
         if (is_numeric($array)) {
             return floatval($array);
         } elseif (is_array($array)) {
-            $result = "";
+            $result = '';
             foreach ($array as $value) {
-                if ($result !== "") {
+                if ($result !== '') {
                     $result .= $glue;
                 }
                 $result .= $this->recursiveImplode($glue, $value);
             }
-            return "[" . $result . "]";
+            return '[' . $result . ']';
         } else {
             return var_export($array, true);
         }
@@ -71,9 +71,9 @@ class DiceRollSymbolRegistry extends SymbolRegistry
     {
         sort($sideArray);
         if ($sideArray == range(1, 100)) {
-            $sideArrayString = "%";
+            $sideArrayString = '%';
         } elseif ($sideArray == range(-1, 1)) {
-            $sideArrayString = "f";
+            $sideArrayString = 'f';
         } elseif (is_numeric(end($sideArray)) && $sideArray == range(1, end($sideArray))) {
             $sideArrayString = end($sideArray);
         } elseif (is_numeric(reset($sideArray)) && $sideArray == range(reset($sideArray), -1)) {
@@ -90,7 +90,7 @@ class DiceRollSymbolRegistry extends SymbolRegistry
 
     protected function add($operand1, $operand2)
     {
-        $log = $this->recursiveImplode(',', $operand1) . "+" . $this->recursiveImplode(',', $operand2);
+        $log = $this->recursiveImplode(',', $operand1) . '+' . $this->recursiveImplode(',', $operand2);
         if (!is_array($operand1) && !is_array($operand2)
             && (is_numeric($operand1) || is_infinite($operand1) || is_nan($operand1))
             && (is_numeric($operand2) || is_infinite($operand2) || is_nan($operand2))) {
@@ -105,16 +105,16 @@ class DiceRollSymbolRegistry extends SymbolRegistry
                 }
             }
         } else {
-            throw new \InvalidArgumentException("Unable to perform operation: " . $log);
+            throw new \InvalidArgumentException('Unable to perform operation: ' . $log);
         }
-        $log .= " = " . $this->recursiveImplode(',', $result);
+        $log .= ' = ' . $this->recursiveImplode(',', $result);
         $this->logCalculation($log);
         return $result;
     }
 
     protected function subtract($operand1, $operand2)
     {
-        $log = $this->recursiveImplode(',', $operand1) . "-" . $this->recursiveImplode(',', $operand2);
+        $log = $this->recursiveImplode(',', $operand1) . '-' . $this->recursiveImplode(',', $operand2);
         if ((is_numeric($operand1) || is_infinite($operand1) || is_nan($operand1))
             && (is_numeric($operand2) || is_infinite($operand2) || is_nan($operand2))) {
             $result = $operand1 - $operand2;
@@ -124,25 +124,25 @@ class DiceRollSymbolRegistry extends SymbolRegistry
                 $result[] = $this->subtract($operand1[$i], $operand2[$i]);
             }
         } else {
-            throw new \InvalidArgumentException("Unable to perform operation: " . $log);
+            throw new \InvalidArgumentException('Unable to perform operation: ' . $log);
         }
-        $log .= " = " . $this->recursiveImplode(',', $result);
+        $log .= ' = ' . $this->recursiveImplode(',', $result);
         $this->logCalculation($log);
         return $result;
     }
 
     protected function negate($operand)
     {
-        $log = "-(" . $this->recursiveImplode(',', $operand) . ")";
+        $log = '-(' . $this->recursiveImplode(',', $operand) . ')';
         $result = $this->multiply(-1, $operand);
-        $log .= " = " . $this->recursiveImplode(',', $result);
+        $log .= ' = ' . $this->recursiveImplode(',', $result);
         $this->logCalculation($log);
         return $result;
     }
 
     protected function multiply($operand1, $operand2)
     {
-        $log = $this->recursiveImplode(',', $operand1) . "*" . $this->recursiveImplode(',', $operand2);
+        $log = $this->recursiveImplode(',', $operand1) . '*' . $this->recursiveImplode(',', $operand2);
         if (!is_array($operand1) && (is_numeric($operand1) || is_infinite($operand1) || is_nan($operand1))
             && !is_array($operand2) && (is_numeric($operand2) || is_infinite($operand2) || is_nan($operand2))) {
             $result = floatval($operand1) * floatval($operand2);
@@ -162,16 +162,16 @@ class DiceRollSymbolRegistry extends SymbolRegistry
                 $result[] = $this->multiply($operand1[$i], $operand2[$i]);
             }
         } else {
-            throw new \InvalidArgumentException("Unable to perform operation: " . $log);
+            throw new \InvalidArgumentException('Unable to perform operation: ' . $log);
         }
-        $log .= " = " . $this->recursiveImplode(',', $result);
+        $log .= ' = ' . $this->recursiveImplode(',', $result);
         $this->logCalculation($log);
         return $result;
     }
 
     protected function divide($operand1, $operand2)
     {
-        $log = $this->recursiveImplode(',', $operand1) . "/" . $this->recursiveImplode(',', $operand2);
+        $log = $this->recursiveImplode(',', $operand1) . '/' . $this->recursiveImplode(',', $operand2);
         if (!is_array($operand1) && (is_numeric($operand1) || is_infinite($operand1) || is_nan($operand1))
             && !is_array($operand2) && (is_numeric($operand2) || is_infinite($operand2) || is_nan($operand2))) {
             $safeOperand2 = ($operand2 == 0 ? (floatval($operand2) * INF) : $operand2);
@@ -192,11 +192,85 @@ class DiceRollSymbolRegistry extends SymbolRegistry
                 $result[] = $this->divide($operand1[$i], $operand2[$i]);
             }
         } else {
-            throw new \InvalidArgumentException("Unable to perform operation: " . $log);
+            throw new \InvalidArgumentException('Unable to perform operation: ' . $log);
         }
-        $log .= " = " . $this->recursiveImplode(',', $result);
+        $log .= ' = ' . $this->recursiveImplode(',', $result);
         $this->logCalculation($log);
         return $result;
+    }
+
+    protected function sum($array) {
+        if (!is_array($array)) {
+            throw new \InvalidArgumentException('Non-array value ' . $this->recursiveImplode(',', $array)
+                . ' passed to function: sum');
+        }
+        $result = 0;
+        foreach ($array as $item) {
+            if (!is_numeric($item)) {
+                throw new \InvalidArgumentException('Non-numeric value ' . $this->recursiveImplode(',', $item)
+                    . ' in values passed to: sum');
+            }
+            $result += $item;
+        }
+        $this->logCalculation('sum(' . $this->recursiveImplode(',', $array) . ') = '
+            . $this->recursiveImplode(',', $result));
+        return $result;
+    }
+
+    protected function top($array) {
+        if (!is_array($array) || count($array) < 2) {
+            throw new \InvalidArgumentException('Too few arguments passed to function: top');
+        } elseif (count($array) > 2) {
+            throw new \InvalidArgumentException('Too many arguments passed to function: top');
+        }
+        $keepCount = reset($array);
+        $items = end($array);
+        if (!is_numeric($keepCount)) {
+            throw new \InvalidArgumentException('Non-numeric value ' . $this->recursiveImplode(',', $keepCount)
+                . ' passed as first argument to function: top');
+        } elseif (!is_array($items)) {
+            throw new \InvalidArgumentException('Non-array value ' . $this->recursiveImplode(',', $items)
+                . ' passed as second argument to function: top');
+        } elseif ($keepCount > count($items)) {
+            throw new \InvalidArgumentException($keepCount . ' numbers were requested but ' . count($items)
+                . ' were passed to function: top');
+        }
+        $log = 'top(' . $keepCount . ', ' . $this->recursiveImplode(',', $items) . ')';
+
+        for ($i = count($items) - $keepCount; $i > 0; $i--) {
+            array_splice($items, array_search(min($items), $items), 1);
+        }
+        $log .= ' = ' . $this->recursiveImplode(',', $items);
+        $this->logCalculation($log);
+        return $items;
+    }
+
+    protected function bottom($array) {
+        if (!is_array($array) || count($array) < 2) {
+            throw new \InvalidArgumentException('Too few arguments passed to function: bottom');
+        } elseif (count($array) > 2) {
+            throw new \InvalidArgumentException('Too many arguments passed to function: bottom');
+        }
+        $keepCount = reset($array);
+        $items = end($array);
+        if (!is_numeric($keepCount)) {
+            throw new \InvalidArgumentException('Non-numeric value ' . $this->recursiveImplode(',', $keepCount)
+                . ' passed as first argument to function: bottom');
+        } elseif (!is_array($items)) {
+            throw new \InvalidArgumentException('Non-array value ' . $this->recursiveImplode(',', $items)
+                . ' passed as second argument to function: bottom');
+        } elseif ($keepCount > count($items)) {
+            throw new \InvalidArgumentException($keepCount . ' numbers were requested but ' . count($items)
+                . ' were passed to function: bottom');
+        }
+        $log = 'bottom(' . $keepCount . ', ' . $this->recursiveImplode(',', $items) . ')';
+
+        for ($i = count($items) - $keepCount; $i > 0; $i--) {
+            array_splice($items, array_search(max($items), $items), 1);
+        }
+        $log .= ' = ' . $this->recursiveImplode(',', $items);
+        $this->logCalculation($log);
+        return $items;
     }
 
     protected function rawDiceRoll($diceCount, $sideArray)
@@ -215,19 +289,19 @@ class DiceRollSymbolRegistry extends SymbolRegistry
     protected function diceRoll($diceCount, $sideArray)
     {
         if (!is_numeric($diceCount) || intval($diceCount) != round($diceCount)) {
-            throw new \InvalidArgumentException("The number of dice to roll must be an integer. "
-                . "Received: " . $this->recursiveImplode(',', $diceCount));
+            throw new \InvalidArgumentException('The number of dice to roll must be an integer. '
+                . 'Received: ' . $this->recursiveImplode(',', $diceCount));
         } elseif (intval($diceCount) <= 0) {
-            throw new \InvalidArgumentException("The number of dice to roll must be greater than 0. "
-                . "Received: " . intval($diceCount));
+            throw new \InvalidArgumentException('The number of dice to roll must be greater than 0. '
+                . 'Received: ' . intval($diceCount));
         } elseif (intval($diceCount) > self::MAXIMUM_ALLOWED_DICE_COUNT) {
-            throw new \InvalidArgumentException("A maximum of " . self::MAXIMUM_ALLOWED_DICE_COUNT
-                . " dice may be rolled at a time (" . intval($diceCount) . " > " . self::MAXIMUM_ALLOWED_DICE_COUNT . ")");
+            throw new \InvalidArgumentException('A maximum of ' . self::MAXIMUM_ALLOWED_DICE_COUNT
+                . ' dice may be rolled at a time (' . intval($diceCount) . ' > ' . self::MAXIMUM_ALLOWED_DICE_COUNT . ')');
         } elseif (!is_array($sideArray)) {
-            throw new \InvalidArgumentException("The sides of dice must be passed as an array. "
-                . "Received: " . $this->recursiveImplode(',', $sideArray));
+            throw new \InvalidArgumentException('The sides of dice must be passed as an array. '
+                . 'Received: ' . $this->recursiveImplode(',', $sideArray));
         } elseif (empty($sideArray)) {
-            throw new \InvalidArgumentException("Dice must have at least one side.");
+            throw new \InvalidArgumentException('Dice must have at least one side.');
         }
 
         $result = null;
@@ -311,7 +385,7 @@ class DiceRollSymbolRegistry extends SymbolRegistry
             return $this->negate($operand);
         }));
         $this->register(new UnaryOperatorEntry('!', UnaryOperatorEntry::POSTFIX, function ($operand) {
-            $log = $this->recursiveImplode(',', $operand) . "!";
+            $log = $this->recursiveImplode(',', $operand) . '!';
             $result = NAN;
             if (!is_array($operand) && is_numeric($operand) && $operand > 0) {
                 if ($operand > 170) { //numbers greater than 170 return INF for !
@@ -322,7 +396,7 @@ class DiceRollSymbolRegistry extends SymbolRegistry
                         $result *= $i;
                     }
                 } else {
-                    throw new \InvalidArgumentException("Unable to perform operation: " . $log);
+                    throw new \InvalidArgumentException('Unable to perform operation: ' . $log);
                 }
             }
             $this->logCalculation($this->recursiveImplode(',', $operand) . '! = ' . $this->recursiveImplode(',', $result));
@@ -374,20 +448,67 @@ class DiceRollSymbolRegistry extends SymbolRegistry
             return self::rawDiceRoll(1, $operand);
         }));
 
-        $this->register(new FunctionEntry("max", function ($array) {
+        $this->register(new FunctionEntry('max', function ($array) {
             if (empty($array)) {
-                throw new \InvalidArgumentException("Too few arguments passed to function: max");
+                throw new \InvalidArgumentException('Too few arguments passed to function: max');
             }
             $result = is_array($array) ? max($array) : $array;
             $this->logCalculation('max(' . $this->recursiveImplode(',', $array) . ') = ' . $this->recursiveImplode(',', $result));
             return $result;
         }));
-        $this->register(new FunctionEntry("min", function ($array) {
+        $this->register(new FunctionEntry('min', function ($array) {
             if (empty($array)) {
-                throw new \InvalidArgumentException("Too few arguments passed to function: min");
+                throw new \InvalidArgumentException('Too few arguments passed to function: min');
             }
             $result = is_array($array) ? min($array) : $array;
             $this->logCalculation('min(' . $this->recursiveImplode(',', $array) . ') = ' . $this->recursiveImplode(',', $result));
+            return $result;
+        }));
+
+        $this->register(new FunctionEntry('top', function ($array) {
+            return self::top($array);
+        }));
+        $this->register(new FunctionEntry('bottom', function ($array) {
+            return self::bottom($array);
+        }));
+
+        $this->register(new FunctionEntry('sum', function ($array) {
+            return self::sum($array);
+        }));
+
+        $this->register(new FunctionEntry('stats', function ($array) {
+            if (!is_array($array) && is_numeric($array)) {
+                $statCount = $array;
+                $keepCount = $array;
+            } elseif (!is_array($array)) {
+                throw new \InvalidArgumentException('Non-array value ' . $this->recursiveImplode(',', $array)
+                    . ' passed to function: stats');
+            } elseif (count($array) < 1) {
+                throw new \InvalidArgumentException('Too few values passed to function: stats');
+            } elseif (count($array) > 2) {
+                throw new \InvalidArgumentException('Too many values passed to function: stats');
+            } else {
+                $statCount = reset($array);
+                if (count($array) == 1) {
+                    $keepCount = count($array);
+                } else {
+                    $keepCount = end($array);
+                }
+            }
+            if (!is_numeric($statCount)) {
+                throw new \InvalidArgumentException('Non-numeric value ' . $this->recursiveImplode(',', $statCount)
+                    . ' passed to function: stats');
+            } elseif (!is_numeric($keepCount)) {
+                throw new \InvalidArgumentException('Non-numeric value ' . $this->recursiveImplode(',', $keepCount)
+                    . ' passed to function: stats');
+            }
+            $result = [];
+            for ($i = 0; $i < $statCount; $i++) {
+                $result[] = self::sum(self::top([3, [self::rawDiceRoll(1,6), self::rawDiceRoll(1,6), self::rawDiceRoll(1,6), self::rawDiceRoll(1,6)]]));
+            }
+            $result = self::top([$keepCount, $result]);
+            $this->logCalculation('stats(' . $this->recursiveImplode(',', $statCount) . ') = '
+                . $this->recursiveImplode(',', $result));
             return $result;
         }));
     }
@@ -414,9 +535,9 @@ class DiceRollSymbolRegistry extends SymbolRegistry
             $sourceLog = [];
         }
 
-        $log = "";
+        $log = '';
         foreach ($sourceLog as $message) {
-            if ($log != "") {
+            if ($log != '') {
                 $log .= $separator;
             }
             $log .= $message;
--- README.md
+++ README.md
@@ -4,22 +4,22 @@ The following direct replacement shortcuts are supported:
 PI d% df dF
 
 The following functions are supported, immediately before a set of parenthesis containing the function arguments:
-max min
+max min top bottom sum
 
 The following unary operators are supported, immediately before their argument:
-\+ \- $r d
+\+ \- \$r d
 
 The following unary operators are supported, immediately after their argument:
-! d% df dF
+\! d% df dF
 
 The following binary operators are supported, immediately between their arguments:
 \+ \- * / d
 
 The following grouping operators are supported to designate arrays:
-[ ]
+\[ \]
 
 The following grouping operators are supported to designate groups such as function arguments and arrays:
-( )
+\( \)
 
 The following operators are supported as separators for the grouping and array operators:
-,
+\,
